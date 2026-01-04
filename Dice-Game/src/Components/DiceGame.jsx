629606857c5c36c7ec5447c650567eacac457624
jennifer hodge <jennifer.hodge@russdarrow.com>
2026-01-04 17:08:13 -0600

    finished react version of dice game

--- Dice-Game/src/Components/DiceGame.jsx
+++ Dice-Game/src/Components/DiceGame.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+/* eslint-disable react-hooks/purity */
+import React, { useState } from "react";
 import "./diceGame.css";
 import Die from "./Die";
 
 export default function DiceGame() {
+  const [diceRoll, setDiceRoll] = useState([1, 2, 3, 4]);
+  const [rollCount, setRollCount] = useState(0);
+
+  const [totalValue, setTotalValue] = useState(0);
+
+  const randomDiceRoll = (max) => {
+    return Math.floor(Math.random() * max) + 1;
+  };
+  const rollHandleClick = () => {
+    const die1 = randomDiceRoll(6);
+    const die2 = randomDiceRoll(6);
+    const die3 = randomDiceRoll(6);
+    const die4 = randomDiceRoll(6);
+    setDiceRoll([die1, die2, die3, die4]);
+
+    setRollCount(rollCount + 1);
+
+    const diceTotal = die1 + die2 + die3 + die4;
+    setTotalValue(diceTotal);
+
+    console.log([die1, die2, die3, die4]);
+  };
+
   return (
     <>
       <div className="diceGameContainer">
@@ -10,15 +34,29 @@ export default function DiceGame() {
           <h2 className="title">🎲 Jenny's Dice Game 🎲</h2>
 
           <div className="dice-grid">
-            <Die />
-            <Die />
-            <Die />
-            <Die />
+            <Die value={diceRoll[0]} />
+            <Die value={diceRoll[1]} />
+            <Die value={diceRoll[2]} />
+            <Die value={diceRoll[3]} />
           </div>
-        </div>
 
-        <div className="sessionArea">
-            
+          <div className="scoreSection">
+            <div className="sectionParts">
+              <p className="yourRoll">Your Roll</p>
+              <p className="bigNum">{totalValue}</p>
+              <p className="rollsThisSes">Rolls this session: {rollCount}</p>
+            </div>
+          </div>
+
+          <div className="rolls">
+            <button
+              type="button"
+              onClick={rollHandleClick}
+              className="rollButton"
+            >
+              Roll Dice
+            </button>
+          </div>
         </div>
       </div>
     </>
--- Dice-Game/src/Components/Die.jsx
+++ Dice-Game/src/Components/Die.jsx
@@ -1,13 +1,36 @@
 import React from "react";
 import "./die.css";
 
-export default function Die() {
+export default function Die(props) {
+  const renderDots = () => {
+    const dots = [];
+
+    const positions = {
+      1: ["center"],
+      2: ["top-left", "bottom-right"],
+      3: ["top-left", "center", "bottom-right"],
+      4: ["top-left", "top-right", "bottom-left", "bottom-right"],
+      5: ["top-left", "top-right", "center", "bottom-left", "bottom-right"],
+      6: [
+        "top-left",
+        "top-right",
+        "middle-left",
+        "middle-right",
+        "bottom-left",
+        "bottom-right",
+      ],
+    };
+
+    positions[props.value]?.forEach((pos, index) => {
+      dots.push(<div key={index} className={`dot ${pos}`}></div>);
+    });
+
+    return dots;
+  };
+
   return (
     <>
-       <div className="dieBox">
-
-
-       </div>
+      <div className="dieBox">{renderDots()}</div>
     </>
   );
 }
